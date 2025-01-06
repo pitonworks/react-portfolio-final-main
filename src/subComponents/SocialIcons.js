885e7fe52dd28e9d4951b232695030445ded00a7
pitonworks <omergungor99@gmail.com>
2025-01-06 06:03:35 +0200

    asd

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -39,7 +39,7 @@ const SocialIcons = (props) => {
         <a
           style={{ color: "inherit" }}
           target="_blank"
-          href={"https://github.com/codebucks27"}
+          href={"https://github.com/pitonworks"}
         >
           <Github
             width={25}
@@ -56,7 +56,7 @@ const SocialIcons = (props) => {
         <a
           style={{ color: "inherit" }}
           target="_blank"
-          href={"https://twitter.com/code_bucks"}
+          href={"https://twitter.com/pitonfaruk"}
         >
           <Twitter
             width={25}
@@ -73,7 +73,7 @@ const SocialIcons = (props) => {
         <a
           style={{ color: "inherit" }}
           target="_blank"
-          href={"https://facebook.com/codebucks27"}
+          href={"https://facebook.com/pitonworks"}
         >
           <Facebook
             width={25}
